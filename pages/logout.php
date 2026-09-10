45fde19392fc379b17533017590f79a907e8fc92
stronck <tibatacamilo2.0@gmail.com>
2026-09-09 20:34:13 -0500

    Subir proyecto AA5 EV02

---
+++ pages/logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+header("Content-Type: application/json; charset=UTF-8");
+
+// Cerrar la sesión actual.
+$_SESSION = [];
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), "", time() - 42000, $params["path"],
+        $params["domain"], $params["secure"], $params["httponly"]);
+}
+
+session_destroy();
+echo json_encode(["success" => "Sesión cerrada exitosamente"]);
+?>

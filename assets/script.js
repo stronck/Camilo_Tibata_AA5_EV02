d7a1236ef3fd25ee740d3dae71039df9fbe5f668
stronck <tibatacamilo2.0@gmail.com>
2026-09-09 21:18:09 -0500

    Agregar comentarios explicativos a script.js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// Esperar a que el HTML termine de cargarse antes de modificar la página.
 document.addEventListener("DOMContentLoaded", function () {
+    // Marcar el documento como cargado para permitir aplicar estilos asociados.
     document.body.classList.add("loaded");
 });
